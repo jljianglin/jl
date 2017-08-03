e80be994d4465467259dc80cd392b5c5978ddc22
jljianglin <861711433@qq.com>
2017-08-03 19:09:49 +0800

    xinde

---
+++ src/main/java/ceshi.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+/**
+ * Created by 刘畅 on 2017/8/3.
+ */
+public class ceshi {
+    @Test
+    public void sayhello() {
+        System.out.println("hello word!");
+    }
+}
