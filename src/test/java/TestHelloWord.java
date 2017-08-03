49d8966e20672fe9fc9c6235052d6c38507e8fa3
jljianglin <861711433@qq.com>
2017-08-03 19:15:35 +0800

    xinde1

---
+++ src/test/java/TestHelloWord.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+/**
+ * Created by 刘畅 on 2017/8/3.
+ */
+public class TestHelloWord {
+
+    @Test
+    public void say() {
+        System.out.println("nihao");
+    }
+}
---
+++ src/test/java/TestNi.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+/**
+ * Created by 刘畅 on 2017/8/3.
+ */
+public class TestNi {
+    @Test
+    public void say() {
+        System.out.println("dui");
+    }
+}
